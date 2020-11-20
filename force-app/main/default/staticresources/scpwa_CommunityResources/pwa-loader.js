187f3811e8c2c61239be980e4f22b6345d1716fc
Georg Wittberger <georg.wittberger@gmail.com>
2020-11-20 22:04:10 +0100

    Remove timestamp from manifest link

--- force-app/main/default/staticresources/scpwa_CommunityResources/pwa-loader.js
+++ force-app/main/default/staticresources/scpwa_CommunityResources/pwa-loader.js
@@ -2,9 +2,8 @@
   // Find community prefix path
   const prefixPathMatch = location.pathname.match(/(^.*?)\/s/i);
   const prefixPath = prefixPathMatch ? prefixPathMatch[1] : '';
-  const currentTimestamp = new Date().getTime();
   const serviceWorkerPath = `${prefixPath}/scpwa_CommunityServiceWorker`;
-  const manifestPath = `${prefixPath}/resource/${currentTimestamp}/scpwa_CommunityResources/manifest.webmanifest`;
+  const manifestPath = `${prefixPath}/resource/scpwa_CommunityResources/manifest.webmanifest`;
 
   // Add web app manifest link
   const manifestLink = document.createElement('link');
